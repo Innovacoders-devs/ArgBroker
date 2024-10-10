a313f61d9bd323282968cf8d6b03cf0db42abe37
Cristian Vargas (Malvadapapa) <cristian.v62@gmail.com>
2024-10-09 22:53:42 -0300

    modificacion del entorno para bajar el venv erroneamente subido/implementacion interfaz DAO

---
+++ Programacion/ArgBroker/src/dao/portafolio_dao.py
@@ -0,0 +1,14 @@
+from dao_interface import DAOInterface
+
+class PortafolioDAO(DAOInterface):
+    def crear(self, objeto):
+        pass
+
+    def obtener(self, id):
+        pass
+
+    def actualizar(self, objeto):
+        pass
+
+    def eliminar(self, id):
+        pass
