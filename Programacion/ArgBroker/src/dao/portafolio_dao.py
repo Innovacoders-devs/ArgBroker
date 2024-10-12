8a714bcee71b8dcba567cb0db9c6cb4ae76f586b
nahir1009 <nahir1009@gmail.com>
2024-10-11 21:44:49 -0300

    CLASE portafolio DAO

--- Programacion/ArgBroker/src/dao/portafolio_dao.py
+++ Programacion/ArgBroker/src/dao/portafolio_dao.py
@@ -1,14 +1,37 @@
 from dao_interface import DAOInterface
 
 class PortafolioDAO(DAOInterface):
-    def crear(self, objeto):
-        pass
+    def crear(self, portafolio):
+        sql= """ 
+        INSERT INTO portafolio (id_inversor,id_accion,cantidad,precio_promedio_compra) 
+        VALUES (%s,%s,%s,%s)
+        """
+        parametro_atributos=(portafolio.id_inversor,portafolio.id_accion,portafolio.cantidad,portafolio.precio_promedio_compra)
+        return self.db_connector.execute_query(sql,parametro_atributos)
 
-    def obtener(self, id):
-        pass
+
+    def obtener(self, portafolio):
+        sql= """ 
+        SELECT * FROM portafolio WHERE id_portafolio=%s
+        """
+        parametro_atributos=(portafolio.id_portafolio,)
+        return self.db_connector.fetch_one(sql,parametro_atributos)
 
     def actualizar(self, objeto):
-        pass
-
+        sql= """ 
+        UPDATE  portafolio 
+        SET id_inversor=%s,id_accion=%s,cantidad=%s,precio_promedio_compra=%s
+        WHERE id_portafolio= %s
+        """
+        parametro_atributos=(portafolio.id_inversor,portafolio.id_accion,portafolio.cantidad,portafolio.precio_promedio_compra)
+       
+        return self.db_connector.execute_query(sql,parametro_atributos)
+ 
     def eliminar(self, id):
-        pass
+        sql= """ 
+        DELETE FROM  portafolio WHERE id_portafolio= %s
+        """
+        parametro_atributos=(portafolio.id_portafolio,)
+        return self.db_connector.execute_query(sql,parametro_atributos)
+       
+        
