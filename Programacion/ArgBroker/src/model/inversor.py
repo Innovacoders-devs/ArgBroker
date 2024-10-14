64bb8f57dafcfe7ec241cd53fa4fde6b1270d0f1
lauzarg <laumica@gmail.com>
2024-10-13 22:39:26 -0300

    se agregan los métodos en orden

--- Programacion/ArgBroker/src/model/inversor.py
+++ Programacion/ArgBroker/src/model/inversor.py
@@ -1,67 +1,90 @@
 import re
 
-
 class Inversor:
-    def __init__(self):
-        def __init__(self, nombre, apellido, email, password, portafolio):
+    def __init__(self, nombre, apellido, cuil, email, password, portafolio):
             self._nombre = nombre
             self._apellido = apellido
+            self._cuil = cuil
             self._email = email
             self.__password = password
             self._portafolio = portafolio
+
+
+    def __str__(self):
+        return f'Inversor: {self._apellido}, {self._nombre} en portafolio {self._portafolio} .'
+
 
     @property
     def nombre(self):
         return self._nombre
 
     @nombre.setter
-    def nombre(self, nom):
-        if not isinstance(nom, str):
+    def nombre(self, nombre):
+        if not isinstance(nombre, str):
             raise ValueError('El nombre debe ser una cadena de texto.')
-        self._nombre = nom
+        self._nombre = nombre
 
     @property
     def apellido(self):
         return self._apellido
 
     @apellido.setter
-    def apellido(self, apell):
-        if not isinstance(apell, str):
+    def apellido(self, apellido):
+        if not isinstance(apellido, str):
             raise ValueError('El apellido debe ser una cadena de texto.')
-        self._apellido = apell
+        self._apellido = apellido
+
+    @property
+    def cuil(self):
+        return self._cuil
+
+    @cuil.setter
+    def cuil(self, cuil):
+        if len(cuil)<11:
+            return f'El cuil ingresado no es válido'
+
 
     @property
     def email(self):
         return self._email
 
     @email.setter
-    def email(self, mail):
-        if '@' not in mail:
+    def email(self, email):
+        if '@' not in email:
             raise ValueError('El correo electrónico debe tener un @')
-        self._email = mail
+        self._email = email
+
+    @staticmethod
+    def validar_correo (email):
+        patron_email = r'[a-zA-ZO-9._%+-]+@[a-zA-ZO-9.-]+\.[a-zA-Z]{2,}$'
+        if re.match(patron_email, email):
+            return "Correo Válido"
+        else:
+            return "Correo no válido - Debe ser: ejemplo@email.com"
+
 
     @property
     def password(self):
         return self.__password
 
     @password.setter
-    def password(self, passw):
-        if not self.es_contrasenia_valida(passw):
+    def password(self, password):
+        if not self.es_contrasenia_valida(password):
             raise ValueError('Está contraseña no cumple con los requisitos de seguridad.')
-        self.__password = passw
+        self.__password = password
+
+    @staticmethod
+    def es_contrasenia_valida(password):
+        if len(password) < 8:
+            return False
 
     @property
     def portafolio(self):
         return self._portafolio
 
     @portafolio.setter
-    def portafolio(self, portfolio):
-        if not isinstance(transaccion, portfolio):
+    def portafolio(self, portafolio):
+        if not isinstance(transaccion, portafolio):
            self.portafolio.append(transaccion)
         else:
             raise ValueError('Esta transacción no corresponde en este portafolio. ')
-
-
-
-
-
