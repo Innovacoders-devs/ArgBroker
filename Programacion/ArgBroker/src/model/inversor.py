f8506a4dd2df0d2819d96907d3255f28aa49402f
Cristian Vargas (Malvadapapa) <cristian.v62@gmail.com>
2024-10-14 09:45:37 -0300

    comentarios de correccion de clase lau =)

--- Programacion/ArgBroker/src/model/inversor.py
+++ Programacion/ArgBroker/src/model/inversor.py
@@ -1,4 +1,4 @@
-import re
+import re  # que es esto?
 
 class Inversor:
     def __init__(self, nombre, apellido, cuil, email, password, portafolio):
@@ -39,7 +39,7 @@ class Inversor:
         return self._cuil
 
     @cuil.setter
-    def cuil(self, cuil):
+    def cuil(self, cuil):  #hay que manejar los errores uniformemente es decir todos con raise Error, aqui no cambia el cuil el setter(si todo esta bien self.cuil == cuil)
         if len(cuil)<11:
             return f'El cuil ingresado no es válido'
         else:
@@ -50,9 +50,21 @@ class Inversor:
     @property
     def email(self):
         return self._email
+    """
+   @email.setter
+    def email(self, email): 
+        if not self.validar_correo(email):  se puede negar la condicion para que quede mas elegante ademas en lugar de validar directamente desde el setter podes llamar a tu funcion validadora de email
+            raise ValueError('El correo electrónico es inválido')
+        self._email = email
 
+    @staticmethod
+    def validar_correo(email):
+        patron_email = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        return re.match(patron_email, email) is not None  
+        # de esta forma retornaria directamente True si esta bien el correo o False si no es correcto
+    """
     @email.setter
-    def email(self, email):
+    def email(self, email): 
         if '@' not in email:
             raise ValueError('El correo electrónico debe tener un @')
         self._email = email
@@ -80,25 +92,38 @@ class Inversor:
     def es_contrasenia_valida(password):
         if len(password) < 8:
             return False
+    #A diferencia de lo que ocurrio con el email aqui lo tenes perfectamente implementado, el evaluador retorna False
+    # y en la password negas la condicion luego pasando a la asignacion del valor, Muy bien!!
 
     @property
     def portafolio(self):
         return self._portafolio
 
     @portafolio.setter
-    def portafolio(self, portafolio):
+    def portafolio(self, portafolio):  # evaluar si la transaccion le pertenece al portafolio me parece mas relacionado con el portafolio que con el inversor quiza le pertenezca a el que opinais?
         if not isinstance(transaccion, portafolio):
            self.portafolio.append(transaccion)
         else:
             raise ValueError('Esta transacción no corresponde en este portafolio. ')
 
+    """ 
+    Ejemplo de autenticacion: 
+    def autenticar(usuario_dao, nombre_usuario, contrasena):
+        try:
+            usuario = usuario_dao.obtener_usuario_por_nombre(nombre_usuario)
+            if usuario and usuario.contrasena == contrasena:
+                return True
+            return False
+        except BaseDatosError as e:
+            raise AutenticacionError(f"Error al autenticar usuario: {str(e)}")"""
 
     def autenticar_inversor (self):
-
+        pass
 
 
     def bloquear(self):
-
+        pass
 
 
     def desbloquear(self):
+        pass
