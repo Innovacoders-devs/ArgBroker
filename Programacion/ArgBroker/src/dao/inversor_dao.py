540b6f3ec0029cac74da03d861e94dd860f236ed
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-10-14 20:22:56 -0300

    Agrega prot With para cerrar conexion/Quitar Tipado-Mejora-Practica-En-DAO

--- Programacion/ArgBroker/src/dao/inversor_dao.py
+++ Programacion/ArgBroker/src/dao/inversor_dao.py
@@ -6,38 +6,41 @@ class InversorDAO(DAOInterface):
     def __init__(self):
         self._db = MySQLConnector()
 
-    def crear(self, inversor: Inversor):
+    def crear(self, inversor):
         query = "INSERT INTO inversores (nombre, apellido, cuil, email, contrasena) VALUES (%s, %s, %s, %s, %s)"
         values = (inversor.nombre, inversor.apellido, inversor.cuil, inversor.email, inversor.contrasena)
-        self._db.execute(query, values)
+        with self._db as db:
+            db.execute(query, values)
 
-    def obtener(self, id_inversor: int) -> Inversor:
+    def obtener(self, id_inversor):
         query = "SELECT * FROM inversores WHERE id = %s"
         result = self._db.fetch_one(query, (id_inversor,))
         if result:
             return Inversor(*result)
         return None
 
-    def actualizar(self, inversor: Inversor):
+    def actualizar(self, inversor):
         query = """
         UPDATE inversores SET nombre = %s, apellido = %s, cuil = %s, email = %s, contrasena = %s 
         WHERE id = %s
         """
         values = (inversor.nombre, inversor.apellido, inversor.cuil, inversor.email, inversor.contrasena, inversor.id)
-        self._db.execute(query, values)
+        with self._db as db:
+            db.execute(query, values)
 
-    def eliminar(self, id_inversor: int):
+    def eliminar(self, id_inversor):
         query = "DELETE FROM inversores WHERE id = %s"
-        self._db.execute(query, (id_inversor,))
+        with self._db as db:
+            db.execute(query, (id_inversor,))
 
-    def obtener_inversor_por_email(self, email: str) -> Inversor:
+    def obtener_inversor_por_email(self, email):
         query = "SELECT * FROM inversores WHERE email = %s"
         result = self._db.fetch_one(query, (email,))
         if result:
             return Inversor(*result)
         return None
 
-    def autenticar(self, email: str, contrasena: str) -> bool:
+    def autenticar(self, email, contrasena):
         query = "SELECT * FROM inversores WHERE email = %s AND contrasena = %s"
         result = self._db.fetch_one(query, (email, contrasena))
         return result is not None
