902aa471d703b75d3f29fbe7a5437c65dbe0302f
KaryQuinteros <karinaq38@gmail.com>
2024-10-15 01:18:30 -0300

    creacion clase portafolio

--- Programacion/ArgBroker/src/model/portafolio.py
+++ Programacion/ArgBroker/src/model/portafolio.py
@@ -1,2 +1,9 @@
 class Portafolio:
-    pass
+    def __init__(self, id_portafolio, id_inversor, id_accion, cantidad, precio_promedio_compra):
+
+        self._id_portafolio = id_portafolio
+        self._id_inversor = id_inversor
+        self._id_accion = id_accion
+        self._cantidad = cantidad
+        self._precio_promedio_compra = precio_promedio_compra
+    
