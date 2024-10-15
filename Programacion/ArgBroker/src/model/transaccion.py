83df5ac579bf5e40889cc2c0168d13b7d82a26b7
MirandaFrancoCBA <mirandafranco.ispc@gmail.com>
2024-10-14 22:33:45 -0300

    Getters y setters

--- Programacion/ArgBroker/src/model/transaccion.py
+++ Programacion/ArgBroker/src/model/transaccion.py
@@ -1,4 +1,5 @@
 class Transaccion:
+
     def __init__(self, id_transaccion, id_inversor, id_accion, tipo, fecha, precio, cantidad, comision):
         self._id_transaccion = id_transaccion
         self._id_inversor = id_inversor
@@ -7,4 +8,72 @@ class Transaccion:
         self._fecha = fecha
         self._precio = precio
         self._cantidad = cantidad
+        self._comision = comision
+
+    def __str__(self):
+        return f"Transaccion numero: {self._id_transaccion}"
+        
+
+    @property
+    def id_transaccion(self):
+        return self._id_transaccion
+
+    @id_transaccion.setter
+    def id_transaccion(self, id_transaccion):
+        self._id_transaccion = id_transaccion
+    
+    @property
+    def id_inversor(self):
+        return self._id_inversor
+
+    @id_inversor.setter
+    def id_inversor(self, id_inversor):
+        self._id_inversor = id_inversor
+    
+    @property
+    def id_accion(self):
+        return self._id_accion
+
+    @id_accion.setter
+    def id_accion(self, id_accion):
+        self._id_accion = id_accion
+    
+    @property
+    def tipo(self):
+        return self._tipo
+
+    @tipo.setter
+    def tipo(self, tipo):
+        self._tipo = tipo
+    
+    @property
+    def fecha(self):
+        return self._fecha
+
+    @fecha.setter
+    def fecha(self, fecha):
+        self._fecha = fecha
+    
+    @property
+    def precio(self):
+        return self._precio
+
+    @precio.setter
+    def precio(self, precio):
+        self._precio = precio
+
+    @property
+    def cantidad(self):
+        return self._cantidad
+
+    @cantidad.setter
+    def cantidad(self, cantidad):
+        self._cantidad = cantidad
+    
+    @property
+    def comision(self):
+        return self._comision
+
+    @comision.setter
+    def comision(self, comision):
         self._comision = comision
