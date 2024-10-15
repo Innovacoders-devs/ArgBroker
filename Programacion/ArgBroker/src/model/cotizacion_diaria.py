2b4ca76fb2485b35a2e9ff0c22f9e5c02352c144
KaryQuinteros <karinaq38@gmail.com>
2024-10-14 23:49:14 -0300

    atributos privados de clase cotizacion

--- Programacion/ArgBroker/src/model/cotizacion_diaria.py
+++ Programacion/ArgBroker/src/model/cotizacion_diaria.py
@@ -1,19 +1,21 @@
 class CotizacionDiaria:
-    def __init__(self, id_cotizacion, id_accion, fecha, ultimo_operado,
-                  cantidad_compra_diaria, precio_compra_actual, precio_venta_actual, 
-                  cantidad_venta_diaria,
-                  valor_apertura, minimo_diario, maximo_diario, valor_cierre):
+    def __init__(self, _id_cotizacion, _id_accion, _fecha, _ultimo_operado,
+                  _cantidad_compra_diaria, _precio_compra_actual, _precio_venta_actual, 
+                  _cantidad_venta_diaria,
+                  _valor_apertura, _minimo_diario, _maximo_diario, _valor_cierre):
         
-        self.id_cotizacion  =   id_cotizacion
-        self.id_accion  =   id_accion
-        self.fecha  =   fecha   
-        self.ultimo_operado =   ultimo_operado
-        self.cantidad_compra_diaria =   cantidad_compra_diaria
-        self.precio_compra_actual   =   precio_compra_actual
-        self.precio_venta_actual    =   precio_venta_actual
-        self.centidad_venta_diaria  =   cantidad_venta_diaria
-        self.valor_apertura =   valor_apertura
-        self.minimo_diario  =   minimo_diario
-        self.maximo_diario  =   maximo_diario
-        self.valor_cierre   =   valor_cierre
+        self._id_cotizacion = _id_cotizacion
+        self._id_accion = _id_accion
+        self._fecha = _fecha   
+        self._ultimo_operado = _ultimo_operado
+        self._cantidad_compra_diaria = _cantidad_compra_diaria
+        self._precio_compra_actual = _precio_compra_actual
+        self._precio_venta_actual = _precio_venta_actual
+        self._cantidad_venta_diaria = _cantidad_venta_diaria
+        self._valor_apertura = _valor_apertura
+        self._minimo_diario = _minimo_diario
+        self._maximo_diario = _maximo_diario
+        self._valor_cierre = _valor_cierre
+    
+           
         
