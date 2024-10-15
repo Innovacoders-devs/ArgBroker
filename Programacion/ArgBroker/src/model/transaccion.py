0dfb791c23dcb40e1849e1987caf39fa3bb3fb04
MirandaFrancoCBA <mirandafranco.ispc@gmail.com>
2024-10-14 21:10:58 -0300

    Creacion de transaccion

--- Programacion/ArgBroker/src/model/transaccion.py
+++ Programacion/ArgBroker/src/model/transaccion.py
@@ -1,2 +1,10 @@
 class Transaccion:
-    pass
+    def __init__(self, id_transaccion, id_inversor, id_accion, tipo, fecha, precio, cantidad, comision):
+        self._id_transaccion = id_transaccion
+        self._id_inversor = id_inversor
+        self._id_accion = id_accion
+        self._tipo = tipo
+        self._fecha = fecha
+        self._precio = precio
+        self._cantidad = cantidad
+        self._comision = comision
