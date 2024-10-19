4b5df2da0b789b92a6a89caacce0677e1a9ed0b7
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-10-19 16:11:09 -0300

    Correccion/actualizar-eliminar; Remover/innecesarios

--- Programacion/ArgBroker/src/dao/inversor_dao.py
+++ Programacion/ArgBroker/src/dao/inversor_dao.py
@@ -49,28 +49,25 @@ class InversorDAO(DAOInterface):
         finally:
             self.__base_de_datos.desconectar_de_base_datos()
 
-    def actualizar(self, inversor):#verificar mapeo, nombres de variables y descoenctar la base de datos luego de la consulta
-        query = """
-        UPDATE inversores SET nombre = %s, apellido = %s, cuil = %s, email = %s, contrasena = %s 
-        WHERE id = %s
-        """ 
-        values = (inversor.nombre, inversor.apellido, inversor.cuil, inversor.email, inversor.contrasena, inversor.id)
-        with self._db as db:
-            db.execute(query, values)
+    def actualizar(self,inversor, nuevos_datos, id):
+        consulta = "UPDATE inversor SET nombre = %s, apellido = %s, cuil = %s, email = %s, contrasena = %s, saldo_cuenta = %s, intentos_fallidos = %s WHERE id = %s"
+        valores_a_insertar = (inversor.nombre, inversor.apellido, inversor.cuil, inversor.email, inversor.contrasena,inversor.saldo_cuenta, inversor.intentos_fallidos, inversor.id)
+        try:
+            self.__base_de_datos.conectar_a_base_datos()
+            self.__base_de_datos.ejecutar_consulta(consulta, valores_a_insertar)
+        except Exception as error:
+            print(f"Error al modificar el inversor: {error}")
+        finally:
+            self.__base_de_datos.desconectar_de_base_datos()
 
     def eliminar(self, id_inversor):
-        query = "DELETE FROM inversores WHERE id = %s"
-        with self._db as db:
-            db.execute(query, (id_inversor,))
-
-    def obtener_inversor_por_email(self, email): #este metodo y buscar por id hacen exactamente lo mismo solo que uno busca por id y el otro por email, quizaconvenga mantener este para buscar el usuario por email al iniciar sesion y luego si volvemos a necesitar un usuario lo buscamos directamente por su email (no creo que sea necesario buscar id ya que una vez que el user inicio sesion este persiste en la app hasta que cierra entonces ya tendriamos su respectivo id)
-        query = "SELECT * FROM inversores WHERE email = %s"
-        result = self._db.fetch_one(query, (email,))
-        if result:
-            return Inversor(*result)
-        return None
-
-    def autenticar(self, email, contrasena): #autenticar es un metodo de la logica de negocio no tendria que estar aqui 
-        query = "SELECT * FROM inversores WHERE email = %s AND contrasena = %s"
-        result = self._db.fetch_one(query, (email, contrasena))
-        return result is not None
+        consulta = "DELETE FROM inversor WHERE id = %s"
+        try:
+            self.__base_de_datos.conectar_a_base_datos()
+            self.__base_de_datos.ejecutar_consulta(consulta, (id_inversor,))
+            return True
+        except Exception as error:
+            print(f"Error al eliminar inversor en la base de datos: {error}")
+            return False
+        finally:
+            self.__base_de_datos.desconectar_de_base_datos()
