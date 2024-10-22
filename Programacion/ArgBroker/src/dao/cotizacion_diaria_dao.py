148d349831942f068d4dc715e35e85e2c5083cd3
lauzarg <laumica@gmail.com>
2024-10-22 08:52:59 -0300

    empiezo la clase cotizacion dao

--- Programacion/ArgBroker/src/dao/cotizacion_diaria_dao.py
+++ Programacion/ArgBroker/src/dao/cotizacion_diaria_dao.py
@@ -1,6 +1,15 @@
-from .dao_interface import DAOInterface
+from dao_interface import DAOInterface
+from model.cotizacion_diaria import CotizacionDiaria
+from src.utils.mysql_connector import MySQLConnector
 
-class CotizacionDiariaDAO(DAOInterface):
+
+class CotizacionDAO(DAOInterface):
+    def __init__(self):
+        self._conector_mysql = MySQLConnector()
+
+    def crear(self, cotizacion_diaria):
+
+
 
     def crear(self, nueva_cotizacion):
         pass
