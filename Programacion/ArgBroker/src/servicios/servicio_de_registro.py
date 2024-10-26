750f2fb7705550b012d0197607c7c6f8baa371d1
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-10-26 02:36:59 -0300

    servicio registro

--- Programacion/ArgBroker/src/servicios/servicio_de_registro.py
+++ Programacion/ArgBroker/src/servicios/servicio_de_registro.py
@@ -1,2 +1,14 @@
-class RegistrarInversor:
-    pass
+from src.modelo.inversor import Inversor
+from src.acceso_a_datos.inversor_dao import InversorDAO
+class ServicioDeRegistro:
+    def __init__(self, inversorDAO):
+        self.__inversor_dao = inversorDAO
+
+    def registrar_usuario(self, nombre, apellido, cuil, email, contrasena):
+        if self.__inversor_dao.obtener_por_email(email):
+            raise ValueError("El email ya está registrado")
+        
+        nuevo_inversor = Inversor(nombre=nombre, apellido=apellido, cuil=cuil, email=email, contrasena=contrasena)
+        
+        self.__inversor_dao.crear(nuevo_inversor)
+        return nuevo_inversor
