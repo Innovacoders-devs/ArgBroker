c7ebeee852e5c70c4f1f05d7fb0a5d2d8b84c280
KaryQuinteros <karinaq38@gmail.com>
2024-10-26 09:39:24 -0300

    se crea clases estado portafolio e historial saldo

--- Programacion/ArgBroker/src/modelo/estado_portafolio.py
+++ Programacion/ArgBroker/src/modelo/estado_portafolio.py
@@ -1,2 +1,53 @@
 class EstadoPortafolio:
-    pass
+
+    def __init__(self, id_estado_portafolio, id_portafolio, id_accion, cantidad, valor_actual):
+        self._id_estado_portafolio = id_estado_portafolio
+        self._id_portafolio = id_portafolio
+        self._id_accion = id_accion
+        self._cantidad = cantidad
+        self._valor_actual = valor_actual
+
+
+    @property
+    def id_estado_portafolio(self):
+        return self._id_estado_portafolio
+
+    @id_estado_portafolio.setter
+    def id_estado_portafolio(self, value):
+        self._id_estado_portafolio = value
+
+
+    @property
+    def id_portafolio(self):
+        return self._id_portafolio
+
+    @id_portafolio.setter
+    def id_portafolio(self, value):
+        self._id_portafolio = value
+
+    
+    @property
+    def id_accion(self):
+        return self._id_accion
+
+    @id_accion.setter
+    def id_accion(self, value):
+        self._id_accion = value
+
+  
+    @property
+    def cantidad(self):
+        return self._cantidad
+
+    @cantidad.setter
+    def cantidad(self, value):
+        self._cantidad = value
+
+  
+    @property
+    def valor_actual(self):
+        return self._valor_actual
+
+    @valor_actual.setter
+    def valor_actual(self, value):
+        self._valor_actual = value
--- Programacion/ArgBroker/src/modelo/historial_saldo.py
+++ Programacion/ArgBroker/src/modelo/historial_saldo.py
@@ -1,2 +1,61 @@
 class HistorialSaldo:
-    pass
+    def __init__(self, id_historial_saldo, id_inversor, fecha, saldo_anterior, saldo_nuevo, motivo):
+        self._id_historial_saldo = id_historial_saldo
+        self._id_inversor = id_inversor
+        self._fecha = fecha
+        self._saldo_anterior = saldo_anterior
+        self._saldo_nuevo = saldo_nuevo
+        self._motivo = motivo
+
+ 
+    @property
+    def id_historial_saldo(self):
+        return self._id_historial_saldo
+
+    @id_historial_saldo.setter
+    def id_historial_saldo(self, value):
+        self._id_historial_saldo = value
+
+
+    @property
+    def id_inversor(self):
+        return self._id_inversor
+
+    @id_inversor.setter
+    def id_inversor(self, value):
+        self._id_inversor = value
+
+   
+    @property
+    def fecha(self):
+        return self._fecha
+
+    @fecha.setter
+    def fecha(self, value):
+        self._fecha = value
+   
+    @property
+    def saldo_anterior(self):
+        return self._saldo_anterior
+
+    @saldo_anterior.setter
+    def saldo_anterior(self, value):
+        self._saldo_anterior = value
+
+    @property
+    def saldo_nuevo(self):
+        return self._saldo_nuevo
+
+    @saldo_nuevo.setter
+    def saldo_nuevo(self, value):
+        self._saldo_nuevo = value
+
+    @property
+    def motivo(self):
+        return self._motivo
+
+    @motivo.setter
+    def motivo(self, value):
+        self._motivo = value
+
+    
